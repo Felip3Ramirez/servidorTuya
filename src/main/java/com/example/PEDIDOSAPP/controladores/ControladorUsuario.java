e46b05ddaaa9c3ff8f6e4bde63f1d5e12b571811
yomapg <yoma0639@gmail.com>
2025-04-10 12:11:45 -0500

    se corrige el error de no crear las tablas

--- src/main/java/com/example/PEDIDOSAPP/controladores/ControladorUsuario.java
+++ src/main/java/com/example/PEDIDOSAPP/controladores/ControladorUsuario.java
@@ -4,7 +4,6 @@ import com.example.PEDIDOSAPP.modelos.Usuario;
 import com.example.PEDIDOSAPP.servicios.UsuarioServicio;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
